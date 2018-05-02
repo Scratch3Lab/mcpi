2cc9ceae885017ce0e1e273455947b71af518135
hidaris <izuo@outlook.com>
2018-05-02 17:07:32 +0800

    add minecraft connect block

--- craft.py
+++ craft.py
@@ -2,56 +2,70 @@
 # encoding: utf-8
 
 from mcpi.minecraft import Minecraft
-rpi_address = "10.10.100.139"
 
-mc = Minecraft.create(address=rpi_address)
-
-
-# while True:
-#     mc.postToChat("Hello world")
+# rpi_address = "10.10.100.229"
 
 
-def move_forward(step):
-    x, y, z = mc.player.getPos()
-    mc.player.setPos(x+step, y, z)
-    return
+class MineCraft:
+    def __init__(self, address):
+        self.id = 'minecraft'
+        self.mc = Minecraft.create(address=address)
 
+    def move_forward(self, step):
+        step = int(step)
+        x, y, z = self.mc.player.getPos()
+        self.mc.player.setPos(x+step, y, z)
+        return {"id": self.id, "topic": "actuator"}
 
-def move_backward(step):
-    x, y, z = mc.player.getPos()
-    mc.player.setPos(x-step, y, z)
-    return
+    def move_backward(self, step):
+        step = int(step)
+        x, y, z = self.mc.player.getPos()
+        self.mc.player.setPos(x-step, y, z)
+        return {"id": self.id, "topic": "actuator"}
 
+    def move_down(self, step):
+        step = int(step)
+        x, y, z = self.mc.player.getPos()
+        self.mc.player.setPos(x, y-step, z)
+        return {"id": self.id, "topic": "actuator"}
 
-def move_down(step):
-    x, y, z = mc.player.getPos()
-    mc.player.setPos(x, y-step, z)
-    return
+    def move_up(self, step):
+        step = int(step)
+        x, y, z = self.mc.player.getPos()
+        self.mc.player.setPos(x, y+step, z)
+        return {"id": self.id, "topic": "actuator"}
 
+    def move_right(self, step):
+        step = int(step)
+        x, y, z = self.mc.player.getPos()
+        self.mc.player.setPos(x, y, z+step)
+        return {"id": self.id, "topic": "actuator"}
 
-def move_up(step):
-    x, y, z = mc.player.getPos()
-    mc.player.setPos(x, y+step, z)
-    return
+    def move_left(self, step):
+        step = int(step)
+        x, y, z = self.mc.player.getPos()
+        self.mc.player.setPos(x, y, z-step)
+        return {"id": self.id, "topic": "actuator"}
 
+    def say(self, word):
+        self.mc.postToChat(word)
+        return {"id": self.id, "topic": "actuator"}
 
-def move_right(step):
-    x, y, z = mc.player.getPos()
-    mc.player.setPos(x, y, z+step)
-    return
+    def is_connected(self, fuzz):
+        if self.mc:
+            return {"id": self.id, "topic": "sensor", "is_connected": True}
+        else:
+            return {"id": self.id, "topic": "sensor", "is_connected": False}
 
-
-def move_left(step):
-    x, y, z = mc.player.getPos()
-    mc.player.setPos(x, y, z-step)
-    return
-
-
-match = {
-    'forward': move_forward,
-    'backward': move_backward,
-    'left': move_left,
-    'right': move_right,
-    'up': move_up,
-    'down': move_down
-}
+    def match(self):
+        env = {
+            'forward': self.move_forward,
+            'backward': self.move_backward,
+            'left': self.move_left,
+            'right': self.move_right,
+            'up': self.move_up,
+            'down': self.move_down,
+            'say': self.say,
+            'is_connected': self.is_connected
+        }
+        return env
